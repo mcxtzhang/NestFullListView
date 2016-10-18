5f6af20ca9b1b48b735f8da728e174f84e4fc400
zhangxutong <zhangxutong@imcoming.cn>
2016-10-18 11:45:12 +0800

    remove footerview

--- app/src/main/java/com/mcxtzhang/cstnorecyclelistview/other/ListViewActivity.java
+++ app/src/main/java/com/mcxtzhang/cstnorecyclelistview/other/ListViewActivity.java
@@ -28,6 +28,7 @@ public class ListViewActivity extends AppCompatActivity {
         initDatas();
         listViewForScrollView = (ListViewForScrollView) findViewById(R.id.lv);
         listViewForScrollView.setAdapter(mAdapter = new LvAdapter(mDatas, this));
+        listViewForScrollView.removeFooterView()
     }
 
     private void initDatas() {
